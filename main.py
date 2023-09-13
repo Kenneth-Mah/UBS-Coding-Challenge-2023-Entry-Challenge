72e417112ab043d1501fb758b8d9011faabb680f
Kenneth-Mah <kennethmah@rocketmail.com>
2023-09-13 17:16:47 +0800

    keys can be accessed like this

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from typing import Dict, List
 def getNextProbableWords(classes: List[Dict],
                          statements: List[str]) -> Dict[str, List[str]]:
   # Fill in your solution here and return the correct output based on the given input
-  classnames = []
-  for someclass in classes:
-    classname = someclass.get
+  classNames = []
+  for classDict in classes:
+    className = list(classDict.keys())[0]
+    classNames.append(className)
+  print(classNames)
+  
   return {}
